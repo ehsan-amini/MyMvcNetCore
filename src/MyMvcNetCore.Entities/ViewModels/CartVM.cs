3a7697348ceea293c34917885295c2df9cd3cf67
ehsan-amini <ehsan.mamini@gmail.com>
2025-09-16 22:49:11 +0200

    Delete src/MyMvcNetCore.Entities/ViewModels directory

--- src/MyMvcNetCore.Entities/ViewModels/CartVM.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MyMvcNetCore.Entities.ViewModels
-{
-    public class CartVM
-    {
-        public IEnumerable<Cart> ShoppingCartList { get; set; }
-        public Order OrderHeader { get; set; }
-    }
-}
